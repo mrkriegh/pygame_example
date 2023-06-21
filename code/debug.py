e0589a0965e5bf3015761a4f972087782da851bb
MrKriegh <james@probabilityandstatistics.com>
2023-06-21 18:02:11 -0400

    Further Fixing Debug Message Placement
    Committed by mrkriegh: Tacking the debug message topright corner to the top right of the display surface so that the message doesn't run off into the aether.

--- code/debug.py
+++ code/debug.py
@@ -4,7 +4,8 @@ font = pygame.font.Font(None,30)
 
 def debug(info,y = 10, x = 1100):
     display_surface = pygame.display.get_surface()
+    (x) = display_surface.get_size()[0] - 10
     debug_surf = font.render(str(info),True,'White')
-    debug_rect = debug_surf.get_rect(topleft = (x,y))
+    debug_rect = debug_surf.get_rect(topright = (x,y))
     pygame.draw.rect(display_surface,'Black',debug_rect)
     display_surface.blit(debug_surf,debug_rect)
