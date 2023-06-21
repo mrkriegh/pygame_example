411360642a8863c92557f1463669c65377b50357
MrKriegh <james@probabilityandstatistics.com>
2023-06-21 19:45:08 -0400

    Basic Audio Menu
    Committed by mrkriegh: Displaying a basic audio menu. Corrected the 'back to previous menu' button to be ESCAPE, since the upgrade menu uses the SPACE button to interact with the currently displayed menu. I then utilized a lot of the same code that is used in upgrade.py to generate the audio menu. I was planning on rotating the orientation of the audio menu, but I decided to put it in the same style as the upgrade menu to keep things a little more uniform. I may have to go back and fix this for the main menu at some point. As of yet, you cannot change the volume values, but that will be covered in issue #2.

--- code/menu.py
+++ code/menu.py
@@ -14,8 +14,8 @@ class InGameMenu:
             },
             'audio': {
                 'text': "Audio",
-                'function': print,
-                'args': "Audio Menu"
+                'function': self.toggle_sub_menu,
+                'args': "audio"
             },
             'story': {
                 'text': "Story",
@@ -33,6 +33,7 @@ class InGameMenu:
         self.width = self.display_surface.get_size()[0] * 0.6
         self.create_menu(self.menu_items)
         self.create_story_pane()
+        self.create_audio_menu()
         
         self.selection_index = 0
         self.selection_time = 0
@@ -79,8 +80,7 @@ class InGameMenu:
             self.item_list.append(item)
             
     def create_audio_menu(self):
-        pass
-        #provide the necessary information to create the audio submenu
+        self.audio_menu = AudioMenu(self.toggle_sub_menu)
     
     def create_story_pane(self):
         text = "Welcome to the arena. You have been chosen to see\n just how far you can make it through wave after wave of\n opponents. You have been allowed to keep your weapons and\n your spells. Good Luck, and fight until you cannot fight\n anymore."
@@ -91,12 +91,14 @@ class InGameMenu:
         self.can_move = False
         self.selection_time = pygame.time.get_ticks()
         if type == "story": self.story_pane.toggle()
+        if type == "audio": self.audio_menu.toggle()
         #toggle displaying one of the submenus or the main menu
 
     def display(self):
         if self.display_sub_menu:
             debug("Showing Sub Menu")
             self.story_pane.display()
+            self.audio_menu.display()
         else: 
             debug("Showing Main Menu")
             self.input()
@@ -133,53 +135,135 @@ class VertMenuItem:
         
 
 class AudioMenu:
-    def __init__(self):
-        pass
-        #create values needed to display and adjust volume settings
+    def __init__(self, toggle_sub_menu):
+        self.display_surface = pygame.display.get_surface()
+        self.font = pygame.font.Font(UI_FONT, UI_FONT_SIZE)
+        self.active = False
+        self.toggle_sub_menu = toggle_sub_menu
+        
+        self.audio_number = len(VOLUME)
+        self.audio_names = list(VOLUME.keys())
+        (self.width,self.height) = self.display_surface.get_size()
+        self.width = self.width // (self.audio_number + 1)
+        self.height *= 0.8
+        #l = self.width * 0.1
+        #t = self.height * 0.1
+        #self.rect = pygame.Rect(l,t,self.width,self.height)
+        # VOLUME = {
+            # 'master': 1,
+            # 'music': 0.5,
+            # 'player_attacks': 0.2,
+            # 'player_spells': 1,
+            # 'monster': 0.6}
+        self.create_items()
+        
+        self.selection_index = 0
+        self.selection_time = 0
+        self.can_move = True
     
     def input(self):
-        pass
-        #create controls for up/down sound selector, right/left level adjustment
+        if not self.can_move: return
+        keys = pygame.key.get_pressed()
+        
+        if keys[pygame.K_UP]:
+            pass#decrease selected options audio level to minimum 0
+        if keys[pygame.K_DOWN]:
+            pass#increase selected options audio level to maximum 1
+        if keys[pygame.K_LEFT]:
+            self.selection_index = (self.selection_index - 1) % self.audio_number
+            self.can_move = False
+            self.selection_time = pygame.time.get_ticks()
+        if keys[pygame.K_RIGHT]:
+            self.selection_index = (self.selection_index + 1) % self.audio_number
+            self.can_move = False
+            self.selection_time = pygame.time.get_ticks()
+        if keys[pygame.K_ESCAPE]:
+            self.can_move = False
+            self.selection_time = pygame.time.get_ticks()
+            self.toggle_sub_menu("audio")
     
     def selection_cooldown(self):
-        pass
-        #same cooldown functionality as above
+        if self.can_move: return
+        current_time = pygame.time.get_ticks()
+        if current_time - self.selection_time >= 100: self.can_move = True
+    
+    def toggle(self):
+        self.active = not self.active
+        self.can_move = False
+        self.selection_time = pygame.time.get_ticks()
     
     def create_items(self):
-        pass
-        #create AudioMenuItems similar to as done above
+        self.item_list = []
+        
+        for item,index in enumerate(range(self.audio_number)):
+            (full_width,top) = self.display_surface.get_size()
+            increment = full_width // self.audio_number
+            left = (item * increment) + (increment - self.width) // 2
+            top = top * 0.1
+            item = AudioMenuItem(left,top,self.width,self.height,index,self.font)
+            self.item_list.append(item)
+        
+        print(f"[Audio Menu Items]:{self.item_list}")
     
     def display(self):
-        pass
-        #dispay one large menu, with each menu item appearing in vertical stack
+        if not self.active: return
+        self.input()
+        self.selection_cooldown()
+        for index,item in enumerate(self.item_list):
+            name = self.audio_names[index]
+            value = list(VOLUME.values())[index]
+            item.display(self.display_surface,self.selection_index,name,value)
 
 class AudioMenuItem:
     def __init__(self,l,t,w,h,index,font):
-        pass
-        #create values needed to display and adjust volume setting
+        self.rect = pygame.Rect(l,t,w,h)
+        self.index = index
+        self.font = font
     
     def display_names(self,surface,name,value,text_color):
-        pass
-        #display volume category name and current value
+        #title
+        title_surf = self.font.render(name,False,text_color)
+        title_rect = title_surf.get_rect(midtop = self.rect.midtop + pygame.math.Vector2(0,20))
+        #value
+        value_surf = self.font.render(f'Value: {int(value)}',False,text_color)
+        value_rect = value_surf.get_rect(midbottom = self.rect.midbottom + pygame.math.Vector2(0,-20))
+        #draw
+        surface.blit(title_surf,title_rect)
+        surface.blit(value_surf,value_rect)
     
     def display_bar(self,surface,value,color):
-        pass
-        #display horizontal scale bar and vertical position bar
+        top = self.rect.midtop + pygame.math.Vector2(0,60)
+        bottom = self.rect.midbottom + pygame.math.Vector2(0,-60)
+        
+        full_height = bottom[1] - top[1]
+        relative_number = value * full_height
+        width = 30
+        height = 10
+        value_rect = pygame.Rect((top[0] - (width / 2)),bottom[1] - relative_number,width,height)
+        
+        pygame.draw.line(surface,color,top,bottom,5)
+        pygame.draw.rect(surface,color,value_rect)
     
     def trigger(self):
         pass
         #on right/left keypress, move vertical position bar and adjust volume settings value by 0.1(+-)
     
     def display(self,surface,selection_num,name,value):
-        pass
-        #display AudioMenuItem on the AudioMenu
+        selected = True if self.index == selection_num else False
+        bg_color = UPGRADE_BG_COLOR_SELECTED if selected else UI_BG_COLOR
+        border_color = UI_BORDER_COLOR_ACTIVE if selected else UI_BORDER_COLOR
+        text_color = TEXT_COLOR_SELECTED if selected else TEXT_COLOR
+        bar_color = BAR_COLOR_SELECTED if selected else BAR_COLOR
+        pygame.draw.rect(surface,bg_color,self.rect)
+        pygame.draw.rect(surface,border_color,self.rect,3)
+        self.display_names(surface,name,value,text_color)
+        self.display_bar(surface,value,bar_color)
 
 class MessagePane:
     def __init__(self, text, toggle_sub_menu):
         self.display_surface = pygame.display.get_surface()
         self.font = pygame.font.Font(UI_FONT, UI_FONT_SIZE)
         self.text = text.split("\n")
-        print(self.text)
         self.active = False
         self.toggle_sub_menu = toggle_sub_menu
         
@@ -197,7 +281,7 @@ class MessagePane:
         if not self.can_move: return
         keys = pygame.key.get_pressed()
         
-        if keys[pygame.K_SPACE]:
+        if keys[pygame.K_ESCAPE]:
             self.can_move = False
             self.selection_time = pygame.time.get_ticks()
             self.toggle_sub_menu("story")
